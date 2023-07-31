7d3a4d38aea5389c4d0fb4418cdc94ee96bc72a4
medeirosrebeca <110829343+medeirosrebeca@users.noreply.github.com>
2023-07-31 15:51:51 +0000

    calculadora

--- calculadora.js
+++ calculadora.js
@@ -3,22 +3,19 @@
 //1. Soma; 2. Subtração; 3. Multiplicação; 4. Divisão
 //Caso seja inserido um número de operação que não exista, o resultado deverá ser 0.
 
-let calculadora;
-calculadora = 1;
-
-switch (calculadora){
-   case 1:
-   console.log ("Soma");
-  break;
-   case 2:
-   console.log ("Subtração");
-   break;
-   case 3:
-   console.log ("Multiplicação");
-   break;
-   case 4:
-   console.log ("Divisão");
-   break;
-   default:
-   console.log ("0");
+function calculadora (num1, num2, entrada){
+      var num1 = prompt ("Digite um número:");
+      var num2 = prompt ("Digite outro número:");
+      var entrada = prompt ("Digite um número de 1 a 4:");
+      if (entrada === 1){
+         console.log (`Você definiu a soma dos números ${num1} + ${num2}`);
+      } else if (entrada === 2) {
+         console.log (`Você definiu a subtração dos números ${num1} - ${num2}`);
+      } else if (entrada === 3){
+         console.log (`Você definiu a multipllicação dos números ${num1} * ${num2}`);
+      }else if (entrada === 4){
+         console.log (`Você definiu a divisão dos números ${num1} / ${num2}`);
+      } else {
+         console.log ("Resultado 0.")
+      }
 }
