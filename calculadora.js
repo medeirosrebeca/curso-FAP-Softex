c6529988283234a713ca80721bf234d3cb25d168
medeirosrebeca <110829343+medeirosrebeca@users.noreply.github.com>
2023-07-31 13:06:13 +0000

    switch

--- calculadora.js
+++ calculadora.js
@@ -3,9 +3,22 @@
 //1. Soma; 2. Subtração; 3. Multiplicação; 4. Divisão
 //Caso seja inserido um número de operação que não exista, o resultado deverá ser 0.
 
-let resultado = calculadora (2,4);
+let calculadora;
+calculadora = 1;
 
-function calculadora(num1, num2){
-   return num1 + num2;
+switch (calculadora){
+   case 1:
+   console.log ("Soma");
+  break;
+   case 2:
+   console.log ("Subtração");
+   break;
+   case 3:
+   console.log ("Multiplicação");
+   break;
+   case 4:
+   console.log ("Divisão");
+   break;
+   default:
+   console.log ("0");
 }
-
