2493b6d995258b9743af8bdf00fb989ec83b96dd
medeirosrebeca <110829343+medeirosrebeca@users.noreply.github.com>
2023-08-01 11:24:47 +0000

    calculadora

--- calculadora.js
+++ calculadora.js
@@ -2,14 +2,11 @@
 e o terceiro será a entrada que definirá a operação a ser executada. Considera a seguinte definição:
 1. Soma; 2. Subtração; 3. Multiplicação; 4. Divisão
 Caso seja inserido um número de operação que não exista, o resultado deverá ser 0.*/
-num1 = 10;
-num2= 2;
-entrada = 1;
 
 function calculadora (num1, num2, entrada){
-      var num1 = Number.prompt ("Digite um número:");
-      var num2 = Number.prompt ("Digite outro número:");
-      var entrada = Number.prompt ("Digite um número de 1 a 4:");
+      var num1 = 10;
+      var num2 = 2;
+      var entrada = 1;
       if (entrada === 1){
          console.log (`Você definiu a soma dos números ${num1} + ${num2}`);
       } else if (entrada === 2) {
