f5b2c53011334a2db6f12b004b1deacbf5e03bac
medeirosrebeca <110829343+medeirosrebeca@users.noreply.github.com>
2023-08-01 11:23:41 +0000

    calculadora

--- calculadora.js
+++ calculadora.js
@@ -1,12 +1,15 @@
-//Fazer uma calculadora de 2 números com três parâmetros: os dois primeiros serão os números da operação
-   //e o terceiro será a entrada que definirá a operação a ser executada. Considera a seguinte definição:
-//1. Soma; 2. Subtração; 3. Multiplicação; 4. Divisão
-//Caso seja inserido um número de operação que não exista, o resultado deverá ser 0.
+/*Fazer uma calculadora de 2 números com três parâmetros: os dois primeiros serão os números da operação
+e o terceiro será a entrada que definirá a operação a ser executada. Considera a seguinte definição:
+1. Soma; 2. Subtração; 3. Multiplicação; 4. Divisão
+Caso seja inserido um número de operação que não exista, o resultado deverá ser 0.*/
+num1 = 10;
+num2= 2;
+entrada = 1;
 
 function calculadora (num1, num2, entrada){
-      var num1 = prompt ("Digite um número:");
-      var num2 = prompt ("Digite outro número:");
-      var entrada = prompt ("Digite um número de 1 a 4:");
+      var num1 = Number.prompt ("Digite um número:");
+      var num2 = Number.prompt ("Digite outro número:");
+      var entrada = Number.prompt ("Digite um número de 1 a 4:");
       if (entrada === 1){
          console.log (`Você definiu a soma dos números ${num1} + ${num2}`);
       } else if (entrada === 2) {
@@ -15,7 +18,6 @@ function calculadora (num1, num2, entrada){
          console.log (`Você definiu a multipllicação dos números ${num1} * ${num2}`);
       }else if (entrada === 4){
          console.log (`Você definiu a divisão dos números ${num1} / ${num2}`);
-      } else {
-         console.log ("Resultado 0.")
-      }
+      } else     
+   return "Resultado 0.";
 }
