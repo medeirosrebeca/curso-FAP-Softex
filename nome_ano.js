5460cebd42db70f5b0b416278fe072c845ba7410
medeirosrebeca <110829343+medeirosrebeca@users.noreply.github.com>
2023-08-05 14:55:32 +0000

    nome e ano

--- nome_ano.js
+++ nome_ano.js
@@ -3,22 +3,17 @@
 completará, no ano atual (2023). Caso o usuário não digite um número ou apareça um inválido no campo do
 ano, o sistema informará o erro e continuará perguntando até que um valor correto seja preenchido.*/
 
-let idade;
-let ano;
-
-let nome = console.log("Qual seu nome completo?");
-console.log('Olá, ' + nome);
-
-ano = parseInt(console.log('Digite o ano de nascimento entre 1922 e 2022?'));
-while (ano < 1922|| ano > 2022 ){
-    try {
-    ano = console.log('Ano inválido. Digite novamente o ano:');
-    if ( ano >= 1922 || ano <= 2022){
-        const anoAtual = new Date().getFullYear();
-        console.log('Você tem', + anoAtual - ano, 'anos');
-    }
-    } catch (error) {
-        alert (error);
-        alert ('Valor invalido. Digite novamente');
-    }
+let nome = prompt("Qual seu nome completo?");
+    while (ano >= 1922 || ano <= 2022 === true){
+        try{
+           let ano = parseInt(prompt('Digite o ano de nascimento:'));
+        if (ano < 1922 || ano > 2022 || isNaN(ano)) {
+            throw new Error ('Ano invalido. Digite novamente:');
+        } else {
+            alert (`Olá, ${nome}! Você tem ${2023-ano} anos`);
+            break;
+        } 
+        } catch (error) {
+            prompt('Inválido.Digite novamente:');
+        }
 }
