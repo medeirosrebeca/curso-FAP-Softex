cd42acc7f77498bdf53f88a1188ee16e7ebe9456
medeirosrebeca <110829343+medeirosrebeca@users.noreply.github.com>
2023-08-06 23:07:22 +0000

    saiu nome e ano

--- nome_ano.js
+++ nome_ano.js
@@ -3,43 +3,23 @@
 completará, no ano atual (2023). Caso o usuário não digite um número ou apareça um inválido no campo do
 ano, o sistema informará o erro e continuará perguntando até que um valor correto seja preenchido.*/
 
-let nome = console.log("Qual seu nome completo?");
-let ano == false;
+let nome = prompt("Qual seu nome completo?");
+let ano = true;
 
-    while (ano >= 1922 || ano <= 2022 == true) {
-        
+    while (true) {
+        ano = parseInt(prompt('Digite o ano de nascimento:'))
         try{
-        ano = parseInt(prompt('Digite o ano de nascimento:'));
-        if (ano < 1922 || ano > 2022 || isNaN(ano)) {
-            throw new Error ('Ano invalido. Digite novamente:');
-        } else {
-            alert (`Olá, ${nome}! Você tem ${2023-ano} anos`);
-            break;
-        } 
-        } catch (error) {
-            prompt('Inválido.Digite novamente:');
+            if (isNaN(ano)) {
+            throw Error ('Digite apenas números:');
+            }
+            if (ano < 1922 || ano > 2022) {
+            alert('Ano inválido. Digite entre 1922 a 2022:');
+            } 
+            else if (ano >= 1922 || ano <= 2022) {
+        console.log(`Olá, ${nome}! Você tem ${2023-ano} anos`);
+        break;
+        }
+        } catch (e) {
+            console.log('Valor informado inválido.');   
+            }
     }
-        }
-
-
-        let nome = prompt("Qual seu nome completo?");
-
-
-        function qualAno(ano) {
-            if (ano < 1922 || ano > 2022 || isNaN(ano)) {
-                throw new Error ('Ano invalido. Digite novamente:');
-            }
-            return ano;
-        }
-            while (ano >= 1922 || ano <= 2022 == true){
-                try{
-                let ano = parseInt(prompt('Digite o ano de nascimento:')); 
-                resposta = qualAno ()
-                }  
-                    alert (`Olá, ${nome}! Você tem ${2023-ano} anos`);
-                while (ano >= 1922 || ano <= 2022){    
-                } 
-                } catch (error) {
-                    prompt('Inválido.Digite novamente:');
-            }
-                
