3749a35027f86df1fdf798015f38c1f8098f3e03
medeirosrebeca <110829343+medeirosrebeca@users.noreply.github.com>
2023-08-09 20:37:18 +0000

    candidato

--- exercicioDoCandidato.js
+++ exercicioDoCandidato.js
@@ -1,7 +1,10 @@
-/*O código deve perguntar se deseja finalizar a votação depois do voto. Caso o número do voto não corresponda a nenhum candidato ou seja branco, ele deve ser tratado como nulo. Se for inserido um texto
-ao invés de número, o código deve retornar uma mensagem para votar novamente.
-Quando a votação for finalizada, o código deverá mostrar o vencedor, aquele com o maior número de votos e, também, a quantidade de votos de cada candidato, os brancos e nulos.*/
-voto = typeof NaN;
+/*O código deve perguntar se deseja finalizar a votação depois do voto. Caso o número do voto não
+corresponda a nenhum candidato ou seja branco, ele deve ser tratado como nulo. Se for inserido um texto
+ao invés de número, o código deve retornar uma mensagem para votar novamente. Quando a votação for
+finalizada, o código deverá mostrar o vencedor, aquele com o maior número de votos e, também, a quantidade
+de votos de cada candidato, os brancos e nulos.*/
+
+let votoFinal;
 const candidato = {
     x: 889,
     y: 847,
@@ -10,24 +13,35 @@ const candidato = {
 };
 while (true) {
     try{
-        let voto = parseInt(prompt('Vote no numero do candidato:'));
-        if (voto != 889 || voto != 847 || voto != 515 || voto != 0){
-            voto = null;
+        let votoCandidato = parseInt(prompt('Vote no numero do candidato:'));
+            if (votoCandidato != 889 && votoCandidato != 847 && votoCandidato != 515 && votoCandidato != 0){
+            votoCandidato = null;
+            }
+            if (votoCandidato = 889){
+            candidato.x;
+            }
+            if (votoCandidato = 847){
+            candidato.y;
+            }
+            if (votoCandidato = 515){
+            candidato.z;
+            }
+            if (votoCandidato = 0){
+            candidato.branco;
+            }
+    } catch (e) {
+        alert('Digite apenas números.');
+    }
+    try {
+    let finalizarVoto = prompt('Deseja finalizar a votação? Digite Sim ou Não:');
+        if (finalizarVoto != 'Sim'){
+            throw Error ('Digite o seu candidato:');
         }
-        if (voto = String){
-            throw Error ('Digite só números:');
-        }
-    let finalizarVoto = prompt('Deseja finalizar a votação? Sim ou Não:');
-        
-        switch (finalizarVoto) {
-            case 'Sim' || 'sim' || 'S' || 's':
-                alert('Votação encerrada.');
-                break;
-            case 'Não':
-                alert('Ok.');
-                break;
-        }
+        if  (finalizarVoto = 'Sim') {
+            console.log ('O candidato com maior votos foi:' + votoFinal);
+            break;
+            }
         } catch (e) {
-            alert('Digite apenas números.');
-        }
+            console.log('Digite novamente o número do seu candidato:');   
+            }
 }
