2473c9c5b4a2aa71bcfdbd994145af4ceb474486
medeirosrebeca <110829343+medeirosrebeca@users.noreply.github.com>
2023-08-14 13:42:41 +0000

    lista encadeada e alg de busca

--- exercicioAlgoritmoDeBusca.js
+++ exercicioAlgoritmoDeBusca.js
@@ -2,3 +2,15 @@
 encontrar o valor 20. Para isso, temos que escolher entre realizar uma busca linear ou binária de acordo
 com a que for mais adequada para essa situação. Codifique a solução mais eficiente para buscar o número
 20 no array.*/
+
+// 1.Busca Linear:
+
+const arrayLinear = [15, 8, 10, 25, 12, 30, 5, 20, 18, 7];
+const verificarNumero = 20;
+
+if (arrayLinear.indexOf(verificarNumero)){
+console.log(`O elemento ${verificarNumero} foi encontrado no indice ${arrayLinear.indexOf(verificarNumero)}.`);
+}
+
+// 2.Busca Binaria:
+
--- exercicioListaEncadeada.js
+++ exercicioListaEncadeada.js
@@ -1,17 +1,52 @@
 /*Crie uma lista encadeada em que cada elemento representa uma pessoa. Ela precisa conter informações
 como nome, idade e referência ao filho dela.*/
 
-class familiaGil {
-    constructor() {
-        this.cabeca = null;
+function No(valor) {
+    this.valor = valor;
+    this.proximo = null;
+  }
+  
+  function inserirInicio(lista, valor) {
+    var novo = new No(valor);
+    if (novo) {
+      novo.proximo = lista;
+      lista = novo;
+    } else {
+      console.log("Erro ao alocar memória");
     }
+    return lista;
+  }
+  
+  function imprimirLista(lista) {
+    var aux = lista;
+    while (aux !== null) {
+      console.log(aux.valor + " |");
+      aux = aux.proximo;
+    }
+  }
+  
+  var Lista = null;
+  Lista = inserirInicio(Lista, 'Bela Gil, 35 anos, Filha');
+  Lista = inserirInicio(Lista, 'Preta Gil, 49 ano, Filha');
+  Lista = inserirInicio(Lista,'Gilberto Gil, 80 anos, Pai');
+  imprimirLista(Lista);
+  
 
-    //Inicio (Push)
-    addFirst(Gilberto){
-        const novoNo = new No (81)
-        novoNo.proximo = this.cabeca;
-        this.cabeca = novoNo;
-    }   
+
+
+
+
+// class familiaGil {
+//     constructor() {
+//         this.cabeca = null;
+//     }
+
+//     //Inicio (Push)
+//     addFirst(Gilberto){
+//         const novoNo = new No (81)
+//         novoNo.proximo = this.cabeca;
+//         this.cabeca = novoNo;
+//     }   
         
-}
-console.log(familiaGil);
+// }
+// console.log(familiaGil);
