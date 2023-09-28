2849ef24d5d04e53a4fa8f8b5b694e5d705159ed
medeirosrebeca <110829343+medeirosrebeca@users.noreply.github.com>
2023-09-28 12:32:59 +0000

    atualizando

---
+++ main.js
@@ -0,0 +1,11 @@
+//Tipos de dados em Typescript: number; null; string; boolean; undefined
+//number:
+var numero;
+numero = 30;
+console.log(numero);
+//null:
+var nome;
+console.log(nome);
+//string:
+//boolean:
+//undefined:
---
+++ main.ts
@@ -0,0 +1,20 @@
+//Tipos de dados em Typescript: number; null; string; boolean; undefined
+
+//number:
+const numero:number;
+numero = 30;
+console.log(numero);
+
+//null:
+var nulo:
+
+
+//string:
+
+
+//boolean:
+
+
+//undefined:
+let nome: string;
+console.log(nome);
