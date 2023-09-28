1387a9b02506101232a0021ea3115d4b99808ad8
medeirosrebeca <110829343+medeirosrebeca@users.noreply.github.com>
2023-09-28 14:01:59 +0000

    typescript

--- main.ts
+++ main.ts
@@ -1,20 +1,73 @@
-//Tipos de dados em Typescript: number; null; string; boolean; undefined
+//Tipos de dados em Typescript: number; null; string; boolean; array; objeto
 
-//number:
-const numero:number;
-numero = 30;
-console.log(numero);
+// //1. number:
+// const numero:number;
+// numero = 30;
+// console.log(numero);
 
-//null:
-var nulo:
-
-
-//string:
-
-
-//boolean:
+// //2. null:
+// var varivavel:null;
+// variavel = null;
+// console.log(variavel);
 
 
-//undefined:
-let nome: string;
-console.log(nome);
+// //3. string:
+// let pessoa:string = 'rebeca';
+// console.log(pessoa);
+
+
+// //4. boolean:
+// let dia:boolean;
+// dia = true;
+// console.log(dia);
+
+
+// //5. array:
+// let frutas:Array;
+// frutas = ['maca, banana, pera'];
+// console.log(frutas);
+
+
+// //6. objeto:
+// let carro = {
+//     marca: 'honda',
+//     modelo: 'civic',
+// };
+// console.log(carro);
+
+
+
+
+
+//Principais métodos de Number:
+// let num:number = 10.5;
+// let valor:string = num.toFixed(2);
+// console.log(`O valor atual é uma: ${typeof valor}`);
+
+// let valor2: number = parseFloat(valor);
+// console.log(`O valor atual é : ${typeof valor2}`);
+
+// console.log(`R$ ${valor2} reais`);
+
+
+//Principais métodos de Strings:
+//1. console.log("rebeca".charAt(3)); //charAt = retorna a letra na posição desejada (lembrando que começa com 0.)
+
+
+// 2. let primeiro = 'guarda';
+// let segundo = 'roupa'
+// let palavra = primeiro.concat(segundo);
+// console.log(palavra); // concat = junta as variaveis.
+
+
+
+// //Criando arrays:
+// const pets: Array<any> = [ "gato", "cão", "vaca", 1, true ]; //uso do 'any' para geral.
+// console.log(pets);
+
+//Principais métodos de Arrays:
+// const cores: Array<string> = ['azul', 'verde', 'amarelo', 'rosa'];
+// console.log(cores.indexOf("verde")); //retorna a posicao da variavel pedida.
+// console.log(cores.lastIndexOf('amarelo'));
+
+
